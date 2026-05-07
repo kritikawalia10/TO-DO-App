dc9689d1ff7c2aaa1a998eccdc3541b1e89a0dcd
Kritika Walia <kritikawalia987@gmail.com>
2026-05-07 13:53:43 +0530

    fix: force dotenv to override system environment variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ override: true });
 const http = require('http');
 const app = require('./app');
 const { connectDB } = require('./config/db');
